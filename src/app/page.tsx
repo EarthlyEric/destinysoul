cd24efeeabbdb568a365da6c35b93a038ef2e1eb
EarthlyEric <earthlyeric6@gmail.com>
2024-06-07 02:32:18 +0800

    ads adjust

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,21 +8,23 @@ export default function Home() {
           <div className="text-black text-6xl font-bold">
             <h1>DestinySoul Studio</h1>
           </div>
-          <div>
+          <div className="place-content-center">
             <button className="text-black border-rose-50 rounded-full p-2">
-              <span>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-6 h-6">
-                  <path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" />
-                </svg>
-              </span>
+              <a href="#ads">
+                <span>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-6 h-6">
+                    <path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" />
+                  </svg>
+                </span>
+              </a>
             </button>
           </div>
         </div>
       </section>
-      <section>
-        <div id="frame" style={{ width: '120px' }}>
-          <iframe data-aa="2330351" src="//ad.a-ads.com/2330351?size=120x60" style={{ width: '120px', height: '60px', border: '0px', padding: '0', overflow: 'hidden', backgroundColor: 'transparent' }}></iframe>
-          <a style={{ display: 'block', textAlign: 'right', fontSize: '12px' }} id="preview-link" href="https://a-ads.com/?partner=2330351">Advertise here</a>
+      <section id="ads" className="w-full flex justify-center items-center py-10">
+        <div id="frame" className="flex flex-col items-center">
+          <iframe data-aa="2330351" src="//ad.a-ads.com/2330351?size=970x90" style={{ width: '970px', height: '90px', border: '0px', padding: '0', overflow: 'hidden', backgroundColor: 'transparent' }}></iframe>
+          <a style={{ display: 'block', textAlign: 'right', fontSize: '12px', marginTop: '10px' }} id="preview-link" href="https://a-ads.com/?partner=2330351">Advertise here</a>
         </div>
       </section>
     </div>
