c555575651deca7c1a8890ee5e9ade303fb7406f
EarthlyEric <earthlyeric6@gmail.com>
2024-06-07 02:20:28 +0800

    add a-ads

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -10,24 +10,28 @@ export default function Navbar(){
             <div className="flex items-center justify-between xl:max-w-7xl xl:mx-auto max-w-full px-[8%] flex-wrap w-full">
                 <Image src="/images/logo_banner.png" width={192} height={64} alt={"Destiny Soul Logo"}/>
                 <button className="text-white lg:hidden block h-6 w-6 cursor-pointer" onClick={()=>close(!open)}>
-                    {open ? (<svg  xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" viewBox="0 0 20 20" fill="currentColor">
-                                <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"/>
-                            </svg>
-                        ) : (<svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16"/>
-                            </svg>)
-                    }
+                    {open ? (
+                        <svg  xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" viewBox="0 0 20 20" fill="currentColor">
+                            <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"/>
+                        </svg>
+                    ) : (
+                        <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16"/>
+                        </svg>
+                    )}
                 </button>
                 <nav className={`${open ? "block":"hidden"} w-full lg:flex lg:item-center lg:w-auto`}>
                     <ul className="font-sans text-base text-center lg:flex lg:justify-between">
                         <li>
                             <a className="lg:px-5 py-2 block text-white hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline rounded-xl" href="./about">
-                            <span className="animation-underline">關於我們</span>
+                                <span className="animation-underline">關於我們</span>
                             </a>
                         </li>
                         <li>
-                            <a className="lg:px-6 py-2 block bg-white text-black hover:text-gary-500 rounded-xl" href="./">More</a>
-                        </li>      
+                            <a className="lg:px-5 py-2 block text-white hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline rounded-xl" href="./portfolio">
+                                <span className="animation-underline">專案</span>
+                            </a>
+                        </li>
                     </ul>
                 </nav>      
             </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,28 @@
+import React from 'react';
+
 export default function Home() {
   return (
     <div>
-      <section id="#title" className="w-full h-screen flex place-content-center items-center bg-[url('/images/bg.gif')] bg-left bg-cover bg-dunes">
+      <section id="title" className="w-full h-screen flex justify-center items-center bg-[url('/images/bg.gif')] bg-left bg-cover bg-dunes">
         <div className="px-10 py-2 grid grid-cols-1 gap-4 h-1/2 place-content-center text-center rounded-xl backdrop-blur-sm bg-white/30">
           <div className="text-black text-6xl font-bold">
             <h1>DestinySoul Studio</h1>
           </div>
           <div>
-            <button className="text-black border-rose-50 rounded-full">
+            <button className="text-black border-rose-50 rounded-full p-2">
               <span>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-6 h-6">
                   <path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" />
                 </svg>
               </span>
             </button>
           </div>
+        </div>
+      </section>
+      <section>
+        <div id="frame" style={{ width: '120px' }}>
+          <iframe data-aa="2330351" src="//ad.a-ads.com/2330351?size=120x60" style={{ width: '120px', height: '60px', border: '0px', padding: '0', overflow: 'hidden', backgroundColor: 'transparent' }}></iframe>
+          <a style={{ display: 'block', textAlign: 'right', fontSize: '12px' }} id="preview-link" href="https://a-ads.com/?partner=2330351">Advertise here</a>
         </div>
       </section>
     </div>
