19b4540fc0469a5c8f674b1537da25925f76f201
EarthlyEric6 <earthlyeric6@gmail.com>
2025-01-31 02:01:51 +0800

    add viewpoint

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <Favicon />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
