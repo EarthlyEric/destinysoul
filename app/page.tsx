ca36b1953393b7d64d256ebdef6c08ecf0c8954f
EarthlyEric <earthlyeric6@gmail.com>
2025-01-30 21:57:20 +0800

    add hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
-
+import { ChevronDownIcon} from "@heroicons/react/16/solid"
 
 export default function HomePage() {
   return (
-    <div>
-      hello world
+    <div className="hero min-h-screen" style={{ backgroundImage: "url('/assets)" }}>
+      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content text-neutral-content text-center">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl font-bold">👋, Hello there !</h1>
+          <p className="mb-5 text-xl font-bold">Welcome to DestinySoul Studio&apos;s Website 🎉</p>
+          <p className="mb-5">
+
+          </p>
+          <button className="btn btn-primary">
+            <ChevronDownIcon className="w-6 h-6 mr-2" />
+            Scroll Down
+            </button>
+        </div>
+      </div>  
     </div>
     );
 }
