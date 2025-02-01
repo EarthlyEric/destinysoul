3297693a65ac0530da988ce6a998ebb6e626313c
EarthlyEric <earthlyeric6@gmail.com>
2025-02-01 18:39:00 +0800

    fix ssr

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import { useTranslations } from "next-intl";
 import { FaAngleDoubleDown } from "react-icons/fa";
 
-
 export default function HomePage() {
   const t = useTranslations('HomePage');
   return (
