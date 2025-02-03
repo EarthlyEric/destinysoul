c01f30505ea60059cd2216e5d420545c539ab74f
EarthlyEric <earthlyeric6@gmail.com>
2025-02-03 23:20:59 +0800

    new design navbar more beautiful

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,62 +1,60 @@
 "use client"
 
-import { HiMenuAlt3, HiX } from "react-icons/hi";
-import Image from 'next/image'
-import Link from 'next/link'
+import { HiMenuAlt3 } from "react-icons/hi";
+import Image from "next/image";
+import Link from "next/link";
 import { useTranslations } from "next-intl";
 import LanguageSwitcher from "./button/languageSwitcher";
 
 export default function Navbar() {
-  const t = useTranslations('Navbar');
+  const t = useTranslations("Navbar");
 
-  const navItem = [
-    { label: t('contact-us'), url: '#contact-us' }
-  ];
+  const navItem = [{ label: t("contact-us"), url: "#contact-us" }];
 
   return (
-    <div className="navbar bg-gray-900 sticky top-0 z-50">
-      <div className="drawer drawer-end">
-        <input id="side-drawer" type="checkbox" className="drawer-toggle" />
-        <div className="drawer-content flex items-center justify-between w-full px-5">
-          {/* Logo */}
-          <div className="navbar-start">
-            <Link href="/" className="btn btn-ghost" style={{ width: '192px', height: '64px' }}>
-              <Image src="/assets/transparent/ds-banner-dark.png" alt="destinysoul logo banner" width={192} height={64} />
-            </Link>
-          </div>
+    <div className="fixed top-4 left-1/2 transform -translate-x-1/2 bg-gray-900 bg-opacity-90 shadow-lg rounded-full z-50 px-6 py-3 w-[90%] max-w-4xl">
+      <div className="flex items-center justify-between">
+        <Link
+          href="/"
+          className="btn btn-ghost flex-shrink-0"
+          style={{ width: "192px", height: "64px" }}
+        >
+          <Image
+            src="/assets/transparent/ds-banner-dark.png"
+            alt="destinysoul logo banner"
+            width={192}
+            height={64}
+          />
+        </Link>
 
-          {/* Desktop Menu */}
-          <div className="navbar-end hidden lg:flex">
-            <ul className="menu menu-horizontal rounded-box px-5">
-              {navItem.map(item => (
-                <li key={item.url}>
-                  <a className="text-lg font-medium" href={item.url}>{item.label}</a>
-                </li>
-              ))}
-              <LanguageSwitcher />
-            </ul>
-          </div>
-          {/* Mobile Toggle Button */}
-          <div className="lg:hidden right-0">
-            <label htmlFor="side-drawer" className="btn btn-ghost drawer-button">
-              <HiMenuAlt3 className="w-6 h-6 text-white" />
-            </label>
-          </div>
-          <input id="side-drawer" type="checkbox" className="drawer-toggle" />
+        {/* Desktop Menu */}
+        <div className="hidden lg:flex items-center space-x-6">
+          <ul className="menu menu-horizontal rounded-box px-5">
+            {navItem.map((item) => (
+              <li key={item.url}>
+                <a className="text-lg font-medium" href={item.url}>
+                  {item.label}
+                </a>
+              </li>
+            ))}
+            <LanguageSwitcher />
+          </ul>
         </div>
 
-        {/* Drawer Side - Mobile Menu */}
-        <div className="drawer-side">
-          <label htmlFor="side-drawer" className="drawer-overlay"></label>
-          <ul className="menu p-4 w-64 min-h-full bg-gray-900 text-white">
-            <li className="flex justify-end">
-              <label htmlFor="side-drawer" className="btn btn-sm btn-ghost">
-                <HiX className="w-6 h-6 text-white" />
-              </label>
-            </li>
-            {navItem.map(item => (
+        {/* Mobile dropdown menu */}
+        <div className="lg:hidden dropdown dropdown-end">
+          <label tabIndex={0} className="btn btn-ghost">
+            <HiMenuAlt3 className="w-6 h-6 text-white" />
+          </label>
+          <ul
+            tabIndex={0}
+            className="dropdown-content menu p-4 mt-3 shadow bg-gray-900 text-white rounded-box w-52"
+          >
+            {navItem.map((item) => (
               <li key={item.url}>
-                <a href={item.url} className="text-lg font-medium">{item.label}</a>
+                <a className="text-lg font-medium" href={item.url}>
+                  {item.label}
+                </a>
               </li>
             ))}
             <LanguageSwitcher />
